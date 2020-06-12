058a5f20eeb699c12124e24cc5489a09c0eb6041
nativedone <adeildoamorim@gmail.com>
2020-06-12 19:52:08 -0300

    Use runApplication method only for web

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 // TODO: check if we need to have index.web.js & index.ios.js & index.android.js
-// to be able to deploy to multiple platform 
+// to be able to deploy to multiple platform
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform } from "react-native";
 
-import App from './App';
+import App from "./App";
 
 // register the app
-AppRegistry.registerComponent('App', () => App);
+AppRegistry.registerComponent("App", () => App);
 
-AppRegistry.runApplication('App', {
-  initialProps: {},
-  rootTag: document.getElementById('root')
-});
+if (Platform.OS === "web") {
+  AppRegistry.runApplication("App", {
+    initialProps: {},
+    rootTag: document.getElementById("root"),
+  });
+}
