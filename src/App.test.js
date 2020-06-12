1d7a4ca1726bb925adbe1248edd2c476878a9941
nativedone <adeildoamorim@gmail.com>
2020-06-12 19:49:48 -0300

    Fix test... yep it works with RN web!

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/This is a RN text/i);
   expect(linkElement).toBeInTheDocument();
 });
