ae3098cd5b9a397fa36e4662e33168659b4244f9
nativedone <adeildoamorim@gmail.com>
2020-06-12 21:58:59 -0300

    Add responsive size to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { size } from '../src/utils/responsiveSize';
 
 function App() {
   return (
     <View style={styles.main}>
-      <Text style={styles.primaryText}>
-         React native web is pretty cool
-      </Text>
+      <Text style={styles.primaryText}>React native web is pretty cool</Text>
       <Text style={styles.secondaryText}>yep, this is a RN text</Text>
     </View>
   );
@@ -15,18 +14,18 @@ function App() {
 const styles = StyleSheet.create({
   main: {
     backgroundColor: "#ccc",
-    paddingTop: 80,
-    paddingBottom: 40,
-    paddingRight: 30,
-    paddingLeft: 60,
+    paddingTop: size["12"],
+    paddingBottom: size["12"],
+    paddingRight:  size["12"],
+    paddingLeft:  size["12"],
   },
   primaryText: {
     color: "blue",
-    fontSize: 40,
+    fontSize: size["48"],
   },
   secondaryText: {
     color: "green",
-    fontSize: 30,
+    fontSize: size["32"],
   },
 });
 
